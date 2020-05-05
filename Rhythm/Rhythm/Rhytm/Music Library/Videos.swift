0b9d5bb0db7515d1a84574cc170aa5f54f397176
chloezql <zl1941@nyu.edu>
2020-05-05 15:10:35 -0400

    add new music playlist files

---
+++ Rhythm/Rhythm/Rhytm/Music Library/Videos.swift
@@ -0,0 +1,27 @@
+//
+//  Videos.swift
+//  TableDatabase
+//
+//  Created by sunshine on 5/3/20.
+//  Copyright © 2020 NYUiOS. All rights reserved.
+//
+
+import Foundation
+
+class Videos {
+    
+    var Title: String?
+    var Link: String?
+    
+    
+    init(Title: String?, Link: String?) {
+        self.Title = Title;
+        self.Link = Link;
+        
+    }
+    
+    
+    
+    
+    
+}
