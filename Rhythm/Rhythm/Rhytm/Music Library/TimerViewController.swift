94161b6b71aece92fb2c380d693b9e3ba937186b
chloezql <zl1941@nyu.edu>
2020-05-11 03:00:51 -0400

    added timer UI, connect timer mp3 player to activity song data. Changed the xib for main page.

--- Rhythm/Rhythm/Rhytm/Music Library/TimerViewController.swift
+++ Rhythm/Rhythm/Rhytm/Music Library/TimerViewController.swift
@@ -19,35 +19,48 @@ class TimerViewController : UIViewController{
     @IBOutlet weak var startBtn: UIButton!
     @IBOutlet weak var pauseBtn: UIButton!
     @IBOutlet weak var timeCount: UILabel!
+    @IBOutlet weak var songImage: UIImageView!
+    @IBOutlet weak var songName: UILabel!
     
-    var audioplayer = AVAudioPlayer()
+    var player = AVPlayer()
     var startTime : Date!
     var endTime : Date!
     var schedule: Activity!
     var difference:Int!
     var timer = Timer()
-    
-    
+    var song = Videos(Title:" temp", Link: "https://firebasestorage.googleapis.com/v0/b/rhythm-4586f.appspot.com/o/songs%2FNils%20Frahm%20-%20You.mp3?alt=media&token=94d6ed5f-7ca6-463f-a94c-5e5dcebbf2c0",Image:"none" )
+    var animating : Bool = false
     override func viewDidLoad(){
         super.viewDidLoad()
+        song = schedule.song
         activityName.text = schedule.name
         //timeCount.text = schedule.color
         startTime = schedule.start_time
         endTime = schedule.end_time
+        songName.text = schedule.song.Title
+        
+        let url = URL(string: schedule.song.Image!)
+        let data = try? Data(contentsOf: url!)
+        songImage.image = UIImage(data: data!)
+        
+        
         calculateDifference()
-        do{
-            let audioPath = Bundle.main.path(forResource: "You", ofType: ".mp3")
-            try audioplayer = AVAudioPlayer(contentsOf: URL(fileURLWithPath: audioPath!))
-        }catch{
-            //ERROR
-        }
         
+        let videoURL = URL(string: song.Link!)
+        player = AVPlayer(url: videoURL!)
+        let playerLayer = AVPlayerLayer(player: player)
         
-        
+        songImage.roundImage()
+        songImage.layer.cornerRadius = songImage.frame.height/2
+        songImage.clipsToBounds = true
+        songImage.layer.borderWidth = 5
+        songImage.layer.borderColor = UIColor.white.cgColor
+
     }
     
     @IBAction func goBack(_ sender: Any) {
         //only going back now for testing purporse
+        player.pause()
         dismiss(animated: true, completion: nil)
        
     }
@@ -61,28 +74,74 @@ class TimerViewController : UIViewController{
          
            //let difference = formatter.string(from: startTime, to: endTime)!
            //print(difference)//output "8 seconds"
-           timeCount.text = String(difference)
+           
+            let hour = (difference / 3600)
+            let minute = (difference % 3600) / 60
+            let second = (difference % 3600) % 60
+          
+        timeCount.text = String(format: "%02i:%02i:%02i", hour, minute, second)
+        
+            
+        
     }
     @IBAction func startCount(_ sender: Any) {
         timer.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(self.Action),userInfo:nil,repeats:true )
+        
+       /*UIView.animate(withDuration: 2.0, animations: {
+           self.songImage.transform = CGAffineTransform(rotationAngle: (180.0 * .pi) / 180.0)
+       })
+ */
+        if(!animating) {
+            animating = true;
+            spinWithOptions(options: UIView.AnimationOptions.curveEaseIn);
+        }
+        
     
+    }
+    
+   
+    
+    func spinWithOptions(options: UIView.AnimationOptions) {
+        UIView.animate(withDuration: 0.5, delay: 0.0, options: options, animations: { () -> Void in
+            let val : CGFloat = CGFloat((M_PI / Double(2.0)));
+            self.songImage.transform = self.songImage.transform.rotated(by: val)
+        }) { (finished: Bool) -> Void in
+
+            if(finished) {
+                if(self.animating){
+                    self.spinWithOptions(options: UIView.AnimationOptions.curveLinear)
+                } else if (options != UIView.AnimationOptions.curveEaseOut) {
+                    self.spinWithOptions(options: UIView.AnimationOptions.curveEaseOut)
+                }
+            }
+
+        }
     }
     
     @IBAction func pauseCount(_ sender: Any) {
         timer.invalidate()
-        audioplayer.stop()
+        player.pause()
+        animating = false
+        
     }
     @objc func Action(){
         difference -= 1
-        timeCount.text = String(difference)
-        if(!audioplayer.isPlaying){
-            audioplayer.play()
+         let hour = (difference / 3600)
+         let minute = (difference % 3600) / 60
+         let second = (difference % 3600) % 60
+                 
+        timeCount.text = String(format: "%02i:%02i:%02i", hour, minute, second)
+        
+        
+        
+        if(player.timeControlStatus != .playing){
+            player.play()
         }
         if(difference == 0){
             timer.invalidate()
             timeCount.text = "Time Up!"
-            audioplayer.stop()
+            player.pause()
         }
         
     }
